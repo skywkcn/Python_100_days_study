3fe9a9a75503cc369b4d4be42d957a7e9357510c
skywkcn <skywkcn@outlook.com>
2021-03-15 20:51:55 +0800

    更新一个例子

---
+++ day07/day07_max2.py
@@ -0,0 +1,12 @@
+def max2(x):
+    m1, m2 = (x[0], x[1]) if x[0] > x[1] else (x[1], x[0])
+    for index in range(2, len(x)):
+        if x[index] > m1:
+            m2 = m1
+            m1 = x[index]
+        elif x[index] > m2:
+            m2 = x[index]
+    print(m1, m2)
+
+
+max2([123, 34, 45, 24, 5, 24, 35, 99, 1067])
